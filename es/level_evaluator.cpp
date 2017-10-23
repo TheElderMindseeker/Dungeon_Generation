feeccb179ab914a292fff21a4c85b68df0e25392
Daniil Alexandrovich <daniil.botnarenku@gmail.com>
2017-10-23 13:37:36 +0300

    Created function calculating reachable area

--- es/level_evaluator.cpp
+++ es/level_evaluator.cpp
@@ -2,11 +2,12 @@
 // Created by daniil on 23.10.17.
 //
 
+#include <stack>
 #include "level_evaluator.hpp"
 
 
 void level_evaluator (int dungeon [LEVEL_SIZE][LEVEL_SIZE], Metainfo &metainfo) {
-    int start_x, start_y;
+    int start_x = 0, start_y = 0;
 
     int total_area = 0;
     for (int y = 0; y < LEVEL_SIZE; y++) {
@@ -31,5 +32,56 @@ void level_evaluator (int dungeon [LEVEL_SIZE][LEVEL_SIZE], Metainfo &metainfo) 
 
 void reachable_area (int dungeon [LEVEL_SIZE][LEVEL_SIZE], Metainfo &metainfo,
                      int start_x, int start_y) {
-    return;
+    int curr_x = start_x, curr_y = start_y;
+    std::stack<std::pair<int, int>> path;
+    bool visited [LEVEL_SIZE][LEVEL_SIZE] = { { false } };
+
+    int reachable = 1;
+    path.push (std::make_pair (start_x, start_y));
+    visited [start_y][start_x] = true;
+    while (! path.empty ()) {
+        if (dungeon [curr_y - 1][curr_x] != VOID && dungeon [curr_y - 1][curr_x] != WALL && ! visited [curr_y - 1][curr_x]) {
+            path.push (std::make_pair (curr_x, curr_y));
+            --curr_y;
+            visited [curr_y][curr_x] = true;
+            ++reachable;
+
+            if (dungeon [curr_y][curr_x] == EXIT)
+                metainfo.exit_reachable = true;
+        }
+        else if (dungeon [curr_y][curr_x + 1] != VOID && dungeon [curr_y][curr_x + 1] != WALL && ! visited [curr_y][curr_x + 1]) {
+            path.push (std::make_pair (curr_x, curr_y));
+            ++curr_x;
+            visited [curr_y][curr_x] = true;
+            ++reachable;
+
+            if (dungeon [curr_y][curr_x] == EXIT)
+                metainfo.exit_reachable = true;
+        }
+        else if (dungeon [curr_y + 1][curr_x] != VOID && dungeon [curr_y + 1][curr_x] != WALL && ! visited [curr_y + 1][curr_x]) {
+            path.push (std::make_pair (curr_x, curr_y));
+            ++curr_y;
+            visited [curr_y][curr_x] = true;
+            ++reachable;
+
+            if (dungeon [curr_y][curr_x] == EXIT)
+                metainfo.exit_reachable = true;
+        }
+        else if (dungeon [curr_y][curr_x - 1] != VOID && dungeon [curr_y][curr_x - 1] != WALL && ! visited [curr_y][curr_x - 1]) {
+            path.push (std::make_pair (curr_x, curr_y));
+            --curr_x;
+            visited [curr_y][curr_x] = true;
+            ++reachable;
+
+            if (dungeon [curr_y][curr_x] == EXIT)
+                metainfo.exit_reachable = true;
+        }
+        else {
+            curr_x = path.top ().first;
+            curr_y = path.top ().second;
+            path.pop ();
+        }
+    }
+
+    metainfo.reachable_area = reachable;
 }
